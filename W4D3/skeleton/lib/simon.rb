56a21ec96762055c404dbd5f706618a5c8ac62df
Gabe Byrne <byrnegr.gmail.com>
2020-06-02 22:08:40 -0400

    simon complete

--- W4D3/skeleton/lib/simon.rb
+++ W4D3/skeleton/lib/simon.rb
@@ -4,19 +4,32 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
 
   end
 
   def play
-
+    until @game_over == true #until the game is over, keep taking turns
+        self.take_turn
+    end
+    self.game_over_message  #once the game is over, show the game over message and reset the game
+    self.reset_game    
   end
 
   def take_turn
+    self.show_sequence #show the sequence
+    self.require_sequence #require the sequence from user
+    if @game_over == false #if the game isn't over show round success and increment sequence length
+      self.round_success_message
+      @sequence_length += 1
+    end  
 
   end
 
   def show_sequence
-
+    self.add_random_color #add random color to sequence
   end
 
   def require_sequence
@@ -24,7 +37,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample #add random color to sequence array
   end
 
   def round_success_message
@@ -36,6 +49,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1 #reset all initialize values once game is complete
+    @game_over = false
+    @seq = []
   end
 end
